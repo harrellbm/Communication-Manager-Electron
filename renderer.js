dab7a1b16beb381573799c2aa1f931c360bd87cc
B Harrell <archerbh6@msn.com>
2020-03-09 20:02:14 -0700

    feat: avenues are added! also some formatting

--- renderer.js
+++ renderer.js
@@ -2,9 +2,8 @@
 // be executed in the renderer process for that window.
 // All of the Node.js APIs are available in this process.
 //this is the js file for the message manager tab
-const ipc = require('electron').ipcRenderer
-const templates = require('./objectTemplate.js')
-
+const ipc = require('electron').ipcRenderer;
+const events = require('events');
 
 //handles event from the save button
 document.getElementById('save').addEventListener("click", saveFile);
@@ -34,34 +33,112 @@ function openFile () {
   document.getElementById('signature').value = file.signature
 };
 
+
 // Adds an avenue to do the DOM
 document.getElementById('add').addEventListener("click", addAvenue);
 var avenueCount = 0;
 function addAvenue () {
-  // Makes the main div to hold an avenue
-  let ave = document.createElement("div");
-  ave.id = `avenue${avenueCount}`;
-  ave.class = "avenue";
-  // Creates simple text input
-  let a = document.createElement("input")
-  ave.appendChild(a)
-  // Creats delete button
-  let b = document.createElement("input");
-  b.type = "button";
-  b.id = `delete${avenueCount}`;
-  b.value = "delete";
-  // Adds dynamic event listener to delete button
-  let id = ave.id
-  b.addEventListener("click", function() {deleteAvenue(id)}) //only works if it was the last one added
-  ave.appendChild(b)
 
-  console.log(ave);
+  //creates main div to hold an individual avenue
+  let ave = document.createElement("div");
+  ave.setAttribute("class", "avenue");
+  ave.setAttribute("id", `avenue${avenueCount}`);
+  
+  // creates drop down list 
+  let dropdown = document.createElement("select");
+  dropdown.setAttribute("class", "dropdown");
+  dropdown.setAttribute("id", `avenue_type${avenueCount}`);
+
+  let option1 = document.createElement("option");
+  option1.setAttribute("value", "option1");
+  option1.innerHTML = "Option 1";
+  dropdown.appendChild(option1);
+
+  let option2 = document.createElement("option");
+  option2.setAttribute("value", "option2");
+  option2.innerHTML = "Option 2";
+  dropdown.appendChild(option2);
+
+  let option3 = document.createElement("option");
+  option3.setAttribute("value", "option3");
+  option3.innerHTML = "Option 3";
+  dropdown.appendChild(option3);
+
+  ave.appendChild(dropdown);
+  
+  // Creates title paragraphs 
+  let description_title = document.createElement("p");
+  description_title.setAttribute("class", "description_title");
+  description_title.setAttribute("id", "description_title");
+  description_title.innerHTML = "Description:";
+  ave.appendChild(description_title);
+
+  let person_title = document.createElement("p");
+  person_title.setAttribute("class", "persons_title");
+  person_title.setAttribute("id", "persons_title");
+  person_title.innerHTML = "Person:";
+  ave.appendChild(person_title);
+
+  let date_title = document.createElement("p");
+  date_title.setAttribute("class", "date_title");
+  date_title.setAttribute("id", "date_title");
+  date_title.innerHTML = "Date:";
+  ave.appendChild(date_title);
+
+  // Creates sent box
+  let sent_box = document.createElement("p");
+  sent_box.setAttribute("class", "sent_box");
+  sent_box.setAttribute("id", `sent_box${avenueCount}`);
+
+  let sent_checkbox = document.createElement("input");
+  sent_checkbox.setAttribute("class", "sent_checkbox");
+  sent_checkbox.setAttribute("id", `sent_checkbox${avenueCount}`);
+  sent_checkbox.setAttribute("type", "checkbox");
+  sent_checkbox.setAttribute("value", "true");
+  sent_box.appendChild(sent_checkbox);
+
+
+  let sent_label = document.createElement("label");
+  sent_label.setAttribute("class", "sent_label");
+  sent_label.setAttribute("id", "sent_label");
+  sent_label.setAttribute("for", "sent_checkbox");
+  sent_label.innerHTML = "Sent";
+  sent_box.appendChild(sent_label)
+
+  ave.appendChild(sent_box);
+
+  // Creates textareas 
+  let description = document.createElement("textarea");
+  description.setAttribute("class", "description");
+  description.setAttribute("id", "description");
+  ave.appendChild(description);
+
+  let persons = document.createElement("textarea");
+  persons.setAttribute("class", "persons");
+  persons.setAttribute("id", "persons");
+  ave.appendChild(persons);
+
+  let dates = document.createElement("textarea");
+  dates.setAttribute("class", "dates");
+  dates.setAttribute("id", "dates");
+  ave.appendChild(dates);
+
+  // Creates and adds dynamic event listener to delete button
+  let deleteBtn = document.createElement("input")
+  deleteBtn.setAttribute("class", "delete")
+  deleteBtn.setAttribute("id", `delete${avenueCount}`)
+  deleteBtn.setAttribute("type", "button")
+  deleteBtn.setAttribute("value", "x")
+  deleteBtn.addEventListener("click", function () {deleteAvenue(ave)}) 
+  ave.appendChild(deleteBtn)
+
+  // Get the main div that holds all the avenues and append the new one
+  console.log("avenue", ave);
   document.getElementById("avenueIn").appendChild(ave);
   ++avenueCount;
 };
 
 // Deletes an avenue from the DOM
-function deleteAvenue (id) {
-  var a = document.getElementById(id)
-  a.parentNode.removeChild(a);
+function deleteAvenue (ave) {
+  ave.parentElement.removeChild(ave)
 };
