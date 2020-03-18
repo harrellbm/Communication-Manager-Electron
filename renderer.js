e7d1bffe8bc7cd5459b055d2b8710b441e924dbc
B Harrell <archerbh6@msn.com>
2020-03-17 17:49:38 -0700

    feat: successfully loads from file

--- renderer.js
+++ renderer.js
@@ -14,6 +14,11 @@ function saveFile () {
   let greetingValue = document.getElementById('greeting').value;
   let contentValue = document.getElementById('content').value;
   let signatureValue = document.getElementById('signature').value;
+  currentMessage.change_title(titleValue);
+  currentMessage.change_greeting(greetingValue);
+  currentMessage.change_content(contentValue);
+  currentMessage.change_signature(signatureValue);
+
   // unpack values from each avenue that is added
   let avenuesValue = document.getElementById('avenueIn').getElementsByClassName('avenue');
   for (i = avenuesValue.length-1; i>=0; i--) {//each iteration goes through one avenue
@@ -23,15 +28,9 @@ function saveFile () {
     let description = avenue.children[5].value;
     let persons = avenue.children[6].value;
     let dates = avenue.children[7].value;
-    console.log('specific elements',avenue, dropdown, sent, description, persons, dates)
+    //console.log('specific elements',avenue, dropdown, sent, description, persons, dates)
     currentMessage.add_avenue(dropdown, description, persons, dates, sent)
     }
-
-  currentMessage.change_title(titleValue);
-  currentMessage.change_greeting(greetingValue);
-  currentMessage.change_content(contentValue);
-  currentMessage.change_signature(signatureValue);
-  //currentMessage.add_avenue(avenuesValue);
   
   ipc.send('save', currentMessage)
 };
@@ -41,30 +40,41 @@ function saveFile () {
 document.getElementById('open').addEventListener("click", openFile);
 function openFile () {
   let file = ipc.sendSync('open-file')
-  console.log('on renderer side' , file)
+  //console.log('on renderer side' , file)
   document.getElementById('title').value = file.title
   document.getElementById('greeting').value = file.greeting
   document.getElementById('content').value = file.content
   document.getElementById('signature').value = file.signature
-  // figure out how to load avenues 
+  // Loads each avenue from the file 
+  for (i = file.avenues.length-1; i>=0; i--) {//each iteration goes through one avenue
+    let avenue = file.avenues[i];
+    //console.log(`avenue${i} to load`, avenue)
+    let dropdown = avenue.avenue_type;
+    let sent = avenue.sent;
+    let description = avenue.description;
+    let persons = avenue.person;
+    let dates = avenue.date;
+    addAvenue(dropdown, sent, description, persons, dates)
+    }
 };
 
 
 // Adds an avenue to do the DOM
 document.getElementById('add').addEventListener("click", addAvenue);
 var avenueCount = 0;
-function addAvenue () {
+function addAvenue (avenue_typeValue='', sentValue='', descriptionValue='', personsValue='', datesValue='') {
 
   //creates main div to hold an individual avenue
   let ave = document.createElement("div");
   ave.setAttribute("class", "avenue");
   ave.setAttribute("id", `avenue${avenueCount}`);
   
-  // creates drop down list 
+  // Creates drop down list 
   let dropdown = document.createElement("select");
   dropdown.setAttribute("class", "dropdown");
   dropdown.setAttribute("id", `avenue_type${avenueCount}`);
-
+  
+  // TODO: need to make options load off of loop 
   let option1 = document.createElement("option");
   option1.setAttribute("value", "option1");
   option1.innerHTML = "Option 1";
@@ -80,26 +90,29 @@ function addAvenue () {
   option3.innerHTML = "Option 3";
   dropdown.appendChild(option3);
 
-  ave.appendChild(dropdown);
+  if(avenue_typeValue != ''){// if creating an avenue that is being pulled from a file set it's value 
+    dropdown.value = avenue_typeValue;
+    }
+  ave.appendChild(dropdown);//add the dropdown menu to the avenue
   
   // Creates title paragraphs 
-  let description_title = document.createElement("p");
+  let description_title = document.createElement("p");// Title for Description 
   description_title.setAttribute("class", "description_title");
   description_title.setAttribute("id", "description_title");
   description_title.innerHTML = "Description:";
-  ave.appendChild(description_title);
+  ave.appendChild(description_title);//add the title to the avenue
 
-  let person_title = document.createElement("p");
+  let person_title = document.createElement("p");// Title for Persons responsible 
   person_title.setAttribute("class", "persons_title");
   person_title.setAttribute("id", "persons_title");
   person_title.innerHTML = "Person:";
-  ave.appendChild(person_title);
+  ave.appendChild(person_title);//add the title to the avenue
 
-  let date_title = document.createElement("p");
+  let date_title = document.createElement("p");// Title for Date 
   date_title.setAttribute("class", "date_title");
   date_title.setAttribute("id", "date_title");
   date_title.innerHTML = "Date:";
-  ave.appendChild(date_title);
+  ave.appendChild(date_title);//add the title to the avenue
 
   // Creates sent box
   let sent_box = document.createElement("p");
@@ -110,32 +123,43 @@ function addAvenue () {
   sent_checkbox.setAttribute("class", "sent_checkbox");
   sent_checkbox.setAttribute("id", `sent_checkbox${avenueCount}`);
   sent_checkbox.setAttribute("type", "checkbox");
-  sent_box.appendChild(sent_checkbox);
-
+  if(sentValue != ''){// if creating an avenue that is being pulled from a file set it's value
+    sent_checkbox.checked = sentValue;
+    }
+  sent_box.appendChild(sent_checkbox);//add check box to the smaller area
 
   let sent_label = document.createElement("label");
   sent_label.setAttribute("class", "sent_label");
   sent_label.setAttribute("id", "sent_label");
   sent_label.setAttribute("for", "sent_checkbox");
   sent_label.innerHTML = "Sent";
-  sent_box.appendChild(sent_label)
+  sent_box.appendChild(sent_label)//add label to the smaller area
 
-  ave.appendChild(sent_box);
+  ave.appendChild(sent_box);//add smaller area to the avenue
 
   // Creates textareas 
   let description = document.createElement("textarea");
   description.setAttribute("class", "description");
   description.setAttribute("id", "description");
+  if(descriptionValue != ''){// if creating an avenue that is being pulled from a file set it's value 
+    description.value = descriptionValue;
+    }
   ave.appendChild(description);
 
   let persons = document.createElement("textarea");
   persons.setAttribute("class", "persons");
   persons.setAttribute("id", "persons");
+  if(personsValue != ''){// if creating an avenue that is being pulled from a file set it's value 
+    persons.value = personsValue;
+    }
   ave.appendChild(persons);
 
   let dates = document.createElement("textarea");
   dates.setAttribute("class", "dates");
   dates.setAttribute("id", "dates");
+  if(datesValue != ''){// if creating an avenue that is being pulled from a file set it's value 
+    dates.value = datesValue;
+    }
   ave.appendChild(dates);
 
   // Creates and adds dynamic event listener to delete button
