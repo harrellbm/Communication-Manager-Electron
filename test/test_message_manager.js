860a5bfa2d08a79c20622e9086de550f37dea094
B Harrell <archerbh6@msn.com>
2020-03-22 16:33:24 -0700

    test: successfully add basic test for open button

--- test/test_message_manager.js
+++ test/test_message_manager.js
@@ -23,12 +23,20 @@ describe('Application launch', function () {
     }
   })
 
-  it('shows an initial window', function () {
-   
-    //console.log(app.client)
-    let test = app.client.click('#save')
-        .then(function () { return app.client.getText('#title')}) 
-    
-    app.client.inspect
-    })
+  it('test clicking open button', async () => {
+    await app.client.waitUntilWindowLoaded();
+    await app.client.click('#open');
+    const title = await app.client.$('#title').getValue();
+    const greeting = await app.client.$('#greeting').getValue();   
+    const content = await app.client.$('#content').getValue();
+    const signature = await app.client.$('#signature').getValue();
+    console.log('title: ', title , '\nGreeting: ', greeting, '\nContent: ', content, '\nSignature: ', signature)
+    expect(title).a('string').that.is.not.equal('');
+    expect(greeting).a('string').that.is.not.equal('');
+    expect(content).a('string').that.is.not.equal('');
+    expect(signature).a('string').that.is.not.equal('');
+  });
+
+
+
 })
