a7e1a442e51c46925bf1d22caefdb349a60ba749
B Harrell <archerbh6@msn.com>
2020-03-23 19:35:17 -0700

    test: successfully able to pull all data from message manager window

--- test/test_message_manager.js
+++ test/test_message_manager.js
@@ -35,8 +35,15 @@ describe('Application launch', function () {
     expect(greeting).a('string').that.is.not.equal('');
     expect(content).a('string').that.is.not.equal('');
     expect(signature).a('string').that.is.not.equal('');
+    let ave = 1
+    while (ave <= 3){
+      let ave_type = await app.client.$(`//div/div/div[${ave}]/select`).getValue();
+      let check = await app.client.$(`//div/div/div[${ave}]/p/input`).getAttribute('checked'); // will return null or true
+      let description = await app.client.$(`//div/div/div[${ave}]/textarea[1]`).getValue();
+      let person = await app.client.$(`//div/div/div[${ave}]/textarea[2]`).getValue();
+      let date = await app.client.$(`//div/div/div[${ave}]/textarea[3]`).getValue();
+      console.log(`Avenue ${ave}\nAvenue Type: `, ave_type, '\nCheck: ', check, '\nDescription: ', description, '\nPerson: ', person, '\nDates: ', date);
+      ++ave
+    };
   });
-
-
-
 })
