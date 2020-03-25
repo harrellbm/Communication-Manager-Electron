c712518d45765cae06e22481480b13e3eb296a7a
B Harrell <archerbh6@msn.com>
2020-03-25 15:56:27 -0700

    test: initial messsage manager integration test - incomplete

--- test/test_message_manager.js
+++ test/test_message_manager.js
@@ -10,7 +10,7 @@ var app = new Application({
     args: [path.join(__dirname, '..')]
   })
 
-describe('Application launch', function () {
+describe('Test Message Manager Functionality', function () {
   this.timeout(10000)
 
   beforeEach(function () {
@@ -23,25 +23,99 @@ describe('Application launch', function () {
     }
   })
 
-   // test clicking save
-   it('should have things written to input/textareas', async () => {
+  // make sure add and delete buttons working 
+  it('should add and delete an avenue', async () => {
+    await app.client.waitUntilWindowLoaded();
+    await app.client.click('#add'); //click add button
+    let avenue = await app.client.isExisting('//div/div/div[1]');
+    expect(avenue).to.be.true;
+    await app.client.click('//div/div/div[1]/input'); //click delete button
+    avenue = await app.client.isExisting('//div/div/div[1]');
+    expect(avenue).to.be.false;
+    })
+
+   // Test full input, save to open loop
+  it('should successfuly implement full input, save, to open loop', async () => {
+    await app.client.waitUntilWindowLoaded();
+    // Set message frame
     await app.client.$('#title').setValue('This is a test Title');
     await app.client.$('#greeting').setValue('This is a test greeting');
     await app.client.$('#content').setValue('This is test content.  Blah Blah Blah.');
     await app.client.$('#signature').setValue('Testing that I can Sign it');
-    const title = await app.client.$('#title').getValue();
-    const greeting = await app.client.$('#greeting').getValue();   
-    const content = await app.client.$('#content').getValue();
-    const signature = await app.client.$('#signature').getValue();
-    console.log('title: ', title , '\nGreeting: ', greeting, '\nContent: ', content, '\nSignature: ', signature)
-    expect(title).is.a('string').that.is.not.equal('');
-    expect(greeting).is.a('string').that.is.not.equal('');
-    expect(content).is.a('string').that.is.not.equal('');
-    expect(signature).is.a('string').that.is.not.equal('');
-  })
+    // Add and fill avenues
+      // Avenue 1
+      await app.client.click('#add');
+      //await app.client.$(`//div/div/div[1]/select`).setValue('1'); //Avenue Type //need to figure out how to set select element
+      //await app.client.$(`//div/div/div[1]/p/input`).setAttribute('checked'); //Check Box // possibly use executeScript function
+      await app.client.$(`//div/div/div[1]/textarea[1]`).setValue('This is a text'); //Description
+      await app.client.$(`//div/div/div[1]/textarea[2]`).setValue('Bob'); //Person
+      await app.client.$(`//div/div/div[1]/textarea[3]`).setValue('12-12-12'); //Date
+      // Avenue 2
+      await app.client.click('#add');
+      //await app.client.$(`//div/div/div[2]/select`).setValue('1'); //Avenue Type //need to figure out how to set select element
+      //await app.client.$(`//div/div/div[2]/p/input`).setAttribute('checked'); //Check Box // possibly use executeScript function
+      await app.client.$(`//div/div/div[2]/textarea[1]`).setValue('This is an email'); //Description
+      await app.client.$(`//div/div/div[2]/textarea[2]`).setValue('Phil, Joe'); //Person
+      await app.client.$(`//div/div/div[2]/textarea[3]`).setValue('9-9-9, 1-1-1'); //Date
+
+    // Save Ui
+    await app.client.click('#save');
+    
+    // Clear Ui
+      // Clear main message frame
+      await app.client.$('#title').setValue('');
+      await app.client.$('#greeting').setValue('');
+      await app.client.$('#content').setValue('');
+      await app.client.$('#signature').setValue('');
+        // Verify elements clear
+        let titleC = await app.client.$('#title').getValue();
+        let greetingC = await app.client.$('#greeting').getValue();   
+        let contentC = await app.client.$('#content').getValue();
+        let signatureC = await app.client.$('#signature').getValue();
+        expect(titleC).is.a('string').that.is.equal('');
+        expect(greetingC).is.a('string').that.is.equal('');
+        expect(contentC).is.a('string').that.is.equal('');
+        expect(signatureC).is.a('string').that.is.equal('');
+      // Clear avenue frame
+        // Clear avenue 1
+        await app.client.click('#delete0'); //click delete button
+          // Verify Avenue deleted 
+          let ave1 = await app.client.isExisting('#avenue0');
+          expect(ave1).to.be.false;
+        // Clear avenue 2
+        await app.client.click('#delete1'); //click delete button
+          // Verify Avenue deleted 
+          let ave2 = await app.client.isExisting('#avenue1');
+          expect(ave2).to.be.false;
+
+    // Open saved ui 
+    await app.client.click('#open');
+      // Main message window 
+      let title = await app.client.$('#title').getValue();
+      let greeting = await app.client.$('#greeting').getValue();   
+      let content = await app.client.$('#content').getValue();
+      let signature = await app.client.$('#signature').getValue();
+        // Verify message frame loaded
+        console.log('title: ', title , '\nGreeting: ', greeting, '\nContent: ', content, '\nSignature: ', signature)
+        expect(title).is.a('string').that.is.equal('This is a test Title');
+        expect(greeting).is.a('string').that.is.equal('This is a test greeting');
+        expect(content).is.a('string').that.is.equal('This is test content.  Blah Blah Blah.');
+        expect(signature).is.a('string').that.is.equal('Testing that I can Sign it');
+      // Avenue window
+      let ave = 1
+      while (ave <= 2){
+        let ave_type = await app.client.$(`//div/div/div[${ave}]/select`).getValue();
+        let check = await app.client.$(`//div/div/div[${ave}]/p/input`).getAttribute('checked'); // will return null or true
+        let description = await app.client.$(`//div/div/div[${ave}]/textarea[1]`).getValue();
+        let person = await app.client.$(`//div/div/div[${ave}]/textarea[2]`).getValue();
+        let date = await app.client.$(`//div/div/div[${ave}]/textarea[3]`).getValue();
+        console.log(`Avenue ${ave}\nAvenue Type: `, ave_type, '\nCheck: ', check, '\nDescription: ', description, '\nPerson: ', person, '\nDates: ', date);
+        ++ave
+        };
+    });
 
   // test clicking open
-  it('test clicking open button', async () => {
+  /*it('test clicking open button', async () => {
     await app.client.waitUntilWindowLoaded();
     await app.client.click('#open');
     // Main message window 
@@ -65,5 +139,5 @@ describe('Application launch', function () {
       console.log(`Avenue ${ave}\nAvenue Type: `, ave_type, '\nCheck: ', check, '\nDescription: ', description, '\nPerson: ', person, '\nDates: ', date);
       ++ave
     };
-  });
+  });*/
 })
