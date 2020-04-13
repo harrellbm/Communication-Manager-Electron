89dc4f2dc0a08cd295f00e3f0b4abc427962edae
B Harrell <archerbh6@msn.com>
2020-04-13 12:43:18 -0700

    style: add strings for needed object methods

--- src/objectTemplate.js
+++ src/objectTemplate.js
@@ -15,6 +15,13 @@ class Initiative {
    // Note: useful built in methods for maps: set(key, value), delete(key), get(key), has(key), clear()
       // keys(), values(), entries(), forEach(), size
    
+   // need change description
+
+   // need add groups 
+   // need get groups
+   // need remove groups 
+   // need clear groups 
+   
    // makes sure that the lowest possible id is assigned to a new avenue 
    id_fill(objects){
       let Id = 0;
@@ -139,7 +146,16 @@ class Goal {
       this.type = '';
       this.reminder = {};
       }
-      //need to add getters and setters as well as tests
+
+      // need change frequency 
+      // need change type
+
+      // need add reminder
+      // need get reminder
+      // need remove reminder 
+      // need clear reminders 
+      // implement date object in reminder
+
   }
 
 // Constructor wrapper for exporting 
@@ -160,14 +176,20 @@ class Message {
    change_title(new_title){
       this.title = new_title
       }
+   
+   // get title
       
    change_greeting(new_greeting){
       this.greeting = new_greeting
       }
 
+   // get greeting
+
    change_content(new_content){ 
       this.content = new_content
       }
+   
+   // get content
    
    change_signature(new_signature){ 
       this.signature = new_signature
@@ -175,7 +197,7 @@ class Message {
       
    // need get avenue ids
    // need add avenue ids
-   // need remove avenue ids
+   // need remove avenue id
    // need clear avenue ids
    };
 
@@ -252,7 +274,8 @@ class Avenue {
       this.sent = new_sent
       }
    
-   // need to get sent 
+   // need to get sent
+   
    // need get message ids
    // need change message id
    // need clear message id
@@ -271,48 +294,6 @@ module.exports = {
 }
 
 /*
-    // TODO: greeting in avenue that can be used instead of default
-
-    // TODO: signature in avenue that can be used instead of ault
-
-     
-     delete_avenue(self, avenue_key){
-        assert avenue_key in self.message_dict.keys(), 'Avenue does not exist'
-        del self.message_dict[avenue_key]
-     }
-
-    // TODO: test get main avenue keys, test get gui keys, returns the avenue specific data
-     get_main_avenue_key(self){
-        avenue_keys = []
-        for avenue in range(0, self.avenue_count):
-            avenue_keys.append(str(avenue))
-        return avenue_keys
-     }
-
-     get_gui_avenue_keys(self){
-        all_avenue_gui_keys = []
-        avenues = self.get_main_avenue_key()
-        for avenue in avenues:
-            temp = self.message_dict[avenue]['gui_keys']
-            for key in temp:
-                all_avenue_gui_keys.append(key)
-        return all_avenue_gui_keys
-     }
-
-     
-     save(self, title, greeting, signature, content, *args){  // TODO: write test
-        // avenue data sent in list of lists through *args
-        self.change_title(title)
-        self.change_greeting(greeting)
-        self.change_signature(signature)
-        self.change_content(content)
-        for avenue in range(0, self.avenue_count):
-            self.change_description(avenue, args[0][avenue][0])
-            self.change_sent(avenue, args[0][avenue][1])
-            self.change_avenue_type(avenue, args[0][avenue][2])
-        print(self.message_dict)
-     }
-
      compose_message_for_avenue(self, avenue){
         // TODO: finish compose message for avenue
         // finsh get avenue will make this step a lot easier
@@ -326,28 +307,6 @@ module.exports = {
         // add method to compose message into single string call it "Compose for Avenue" that takes  a Message and Avenue
         // then pulls unique greeting and signature oF applicable and puts it all into a condensed dictionary
      }
-     */
-/*
-
-
-
-class Campaign{
-     constructor (self, *args){  // possibly **wkargs better?
-        self.messages = []
-        for message in args:
-            self.messages.append(message)
-     }
-     add_message(self, *args){  // possibly **wkargs better?
-        self.messages.append(args)
-        // TODO: add test for this method
-     }
-
-     get_message(self){
-        return self.messages
-     }
-
-    // TODO: delete message
-    }
 
 class Profile{
      constructor(){}
