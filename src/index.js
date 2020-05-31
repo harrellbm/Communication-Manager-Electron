97ac71b3aec7ab52bc4c7b7c13c5aa6229bbd534
B Harrell <archerbh6@msn.com>
2020-05-31 16:54:14 -0700

    feat: basic modal ui for avenue

--- src/index.js
+++ src/index.js
@@ -1219,3 +1219,41 @@ document.getElementById('next').addEventListener('click', function (event) {
   document.getElementById('year').value = `${end.getFullYear()}`;
   document.getElementById('month').value = `${start.getMonth() + 1}` + '.' + `${start.getDate()}` + ' - ' + `${end.getMonth() + 1}` + '.' + `${end.getDate()}`;
 });
+
+/* ---- Unified modal popup to add Avenues across tabs ---- */
+
+// Get the modal
+var modal = document.getElementById("myModal");
+
+// Get the button that opens the modal
+var btn = document.getElementById("addAve");
+
+// Get the <span> element that closes the modal
+var span = document.getElementsByClassName("close")[0];
+
+// When the user clicks on the button, open the modal
+btn.onclick = function() {
+   // Set dropdown options from list held in the initiative object 
+   let dropdown = document.getElementById('aveDropModal')
+   let options = currentInitiative.avenue_types;
+   for (i in options){
+     let opElem = document.createElement("option");
+     let opText = currentInitiative.avenue_types[i]
+     opElem.setAttribute("value", `${opText}`);
+     opElem.innerHTML = `${opText}`;
+     dropdown.appendChild(opElem);
+     }
+  modal.style.display = "block";
+}
+
+// When the user clicks on <span> (x), close the modal
+span.onclick = function() {
+  modal.style.display = "none";
+}
+
+// When the user clicks anywhere outside of the modal, close it
+window.onclick = function(event) {
+  if (event.target == modal) {
+    modal.style.display = "none";
+  }
+}
