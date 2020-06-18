1333fcfc6dfccc767ff58ae0aef3edfbf83cca91
B Harrell <archerbh6@msn.com>
2020-06-17 19:59:22 -0700

    style: finished mess tab ave style for usability and look

--- src/index.js
+++ src/index.js
@@ -597,6 +597,17 @@ function addAve (event='', aveId='', location='avenueIn', modalAddSent=false, mo
     }
     ave.appendChild(date);
   
+  
+    // Adds an Icon to note that avenue is linked with goal 
+    let goalIcn = document.createElement("div");
+    //goalIcn.setAttribute("class", "goalIcn");
+    goalIcn.innerHTML = '<svg width="20" height="20" viewBox="0 0 20 20" fill="none" xmlns="http://www.w3.org/2000/svg"> <path class="goalIcn" fill-rule="evenodd" clip-rule="evenodd" d="M18 13V5C18 3.89543 17.1046 3 16 3H4C2.89543 3 2 3.89543 2 5V13C2 14.1046 2.89543 15 4 15H7L10 18L13 15H16C17.1046 15 18 14.1046 18 13ZM5 7C5 6.44772 5.44772 6 6 6H14C14.5523 6 15 6.44772 15 7C15 7.55228 14.5523 8 14 8H6C5.44772 8 5 7.55228 5 7ZM6 10C5.44772 10 5 10.4477 5 11C5 11.5523 5.44772 12 6 12H9C9.55229 12 10 11.5523 10 11C10 10.4477 9.55229 10 9 10H6Z" fill="#4A5568"/> </svg>';
+    // If avenue is connected to a goal add special icon  
+    if (aveLoad.goal_id != '') {
+      ave.appendChild(goalIcn);
+    };
+    
+
     // Creates and adds dynamic event listener to delete button
     let deleteBtn = document.createElement("span");
     deleteBtn.setAttribute("class", "aveDelete");
@@ -604,7 +615,7 @@ function addAve (event='', aveId='', location='avenueIn', modalAddSent=false, mo
     deleteBtn.innerHTML = '&times;'; 
     // If avenue is connected to a goal disable the delete button 
     if (aveLoad.goal_id != '') {
-      deleteBtn.setAttribute("style", "display: none;")
+      deleteBtn.setAttribute("style", "display: none;");
     }
     deleteBtn.addEventListener("click", function () {deleteAveMess(ave)}); 
     ave.appendChild(deleteBtn);
